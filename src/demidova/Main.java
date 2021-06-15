c1e5639a031f74dd19b8cf68aec17054b9d197ad
MariaDemidova <demidova.ma@yandex.ru>
2021-06-15 19:43:59 +0300

    Initial commit

---
+++ src/demidova/Main.java
@@ -0,0 +1,61 @@
+package demidova;
+
+public class Main {
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2;
+
+    public static void main(String[] args) {
+      manageTwoArrs();
+      manageOneArr();
+    }
+
+
+    public static void manageOneArr(){
+        float[] arr = new float[SIZE];
+        fillArr(arr);
+        long a = System.currentTimeMillis();
+        calc(arr);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void manageTwoArrs(){
+        float[] arr = new float[SIZE];
+        fillArr(arr);
+        float[] a = new float[HALF];
+        float[] b = new float[HALF];
+        long startTimeA = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a, 0, HALF);
+        System.arraycopy(arr, HALF, b, 0, HALF);
+
+        new Thread(() -> calc(a)).start();
+        new Thread(() -> calc(b)).start();
+
+       /* new Thread(() -> {
+            float[] a1 = calc(a);
+            System.arraycopy(a1, 0, a, 0, a1.length);
+        }).start();
+
+        new Thread(() -> {
+            float[] b1 = calc(b);
+            System.arraycopy(b1, 0, b, 0, b1.length);
+        }).start();
+*/
+        System.arraycopy(a, 0, arr, 0, HALF);
+        System.arraycopy(b, 0, arr, HALF, HALF);
+        System.out.println(System.currentTimeMillis() - startTimeA);
+
+    }
+    public static void fillArr(float[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1.0f;
+        }
+    }
+
+    public  static float[] calc(float[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        return  arr;
+    }
+
+}
